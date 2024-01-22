7deaeb0e6814ee33f0a6d110566d2e09a4c11eed
Gabriel Monteiro <gabrielm.reserva@gmail.com>
2024-01-22 12:55:38 -0300

    adicionado o front do site

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Designer Gráfico","Front-End Dev","AWS Engineer",``],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
